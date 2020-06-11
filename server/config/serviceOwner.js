6b8ffc690a795e2d2f7296b723d7016f56a71e03
ahmedatef00 <ahmedatef62437@gmail.com>
2020-06-11 22:47:58 +0200

    Register as a user a service provider, login and  authorization on rails

---
+++ server/config/serviceOwner.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+
+const serviceOwner = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decode = jwt.verify(token, process.env.secret);
+    const user = await User.findById(decode._id);
+    if (!user.isadmin) throw new Error();
+    next();
+  } catch (error) {
+    res.status(401).send({ message: "UnAuthorized it is an admin Area" });
+  }
+};
+
+module.exports = serviceOwner;
