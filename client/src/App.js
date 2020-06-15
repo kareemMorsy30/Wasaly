13ab0f1f5a51480660ba485b545c365f0051e8fe
mohamedadham <mohamedadham27@gmail.com>
2020-06-15 20:05:38 +0200

    order service form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { Router, Switch, Route } from 'react-router-dom'
 import CreateProduct from './components/product owner/createProduct'
 import UpdateProduct from './components/product owner/updateProduct'
 import ListProducts  from './components/product owner/listProducts';
+import ServiceOrderForm from './components/customer/serviceForm';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route exact path="/products" component={ListProducts} />
             <Route exact path="/products/create" component={CreateProduct} />
             <Route exact path="/products/:id/edit" component={UpdateProduct} />
+            <Route exact path="/" component={ServiceOrderForm} />
           </Switch>
         </div>
       </div>
--- server/controllers/order.js
+++ server/controllers/order.js
@@ -10,14 +10,14 @@ exports.listOrders = async (req, res, next) => {
     }
 }
 
-exports.chaneOrderStatus = async(req, res, next) => {
+exports.chaneOrderStatus = async (req, res, next) => {
     try {
         const orderID = req.params.id
         const userID = req.user._id
 
-        const order = await Order.findOne({_id:orderID, productOwner: userID}).exec()
-        !order ? order = await Order.findOne({_id:orderID, service: userID}).exec():
-        res.status(401).send({ message: "Order Not Found" })
+        const order = await Order.findOne({ _id: orderID, productOwner: userID }).exec()
+        !order ? order = await Order.findOne({ _id: orderID, service: userID }).exec() :
+            res.status(401).send({ message: "Order Not Found" })
 
         req.body.status && (product.status = req.body.status)
 
@@ -26,7 +26,28 @@ exports.chaneOrderStatus = async(req, res, next) => {
 
     } catch (err) {
         next(err)
-    }   
-   
+    }
 
+
+}
+
+exports.saveOrderForService = async (req, res, next) => {
+    try {
+        const orderID = req.params.id
+        const userID = req.user._id
+        const {
+            serviceOwnerID,
+            from,
+            to,
+            item,
+            amount,
+            cost,
+            description } = req.body
+        const status="Pending"
+        await new Order({to, from, item, amount, cost, description, service: serviceOwnerID, status})
+        res.send("Status Updated Successfully")
+
+    } catch (err) {
+        next(err)
+    }
 }
