6e05160f5f47481736866944c5b2c6d4644b4e6d
kareemMorsy30 <kareemmorsy30@gmail.com>
2020-06-18 05:36:19 +0200

    From and to delivery order form inputs geocoding, suggestions and required validation

--- client/src/components/customer/serviceForm.js
+++ client/src/components/customer/serviceForm.js
@@ -4,103 +4,122 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useForm } from "react-hook-form";
 import '../../styles/form.scss';
 import { getAvailableTransportations } from '../../endpoints/order';
+import { getGeoLocation } from '../../endpoints/geocoding';
 import '../../styles/delivery-section.scss';
 
 const ServiceOrderForm = (props) => {
-    const [item, setItem] = useState([])
-    const [amount, setAmount] = useState()
-    const { register, errors, handleSubmit } = useForm();
+    const [item, setItem] = useState('')
+    const [amount, setAmount] = useState(1)
     const [description, setDescription] = useState('')
-    const [transportation, setTransportation] = useState('Transportation way');
+    const [transportation, setTransportation] = useState('');
     const [from, setFrom] = useState({
-        area: ''
+        area: '',
+        street: '',
+        city: '',
+        longitude: 0,
+        latitude: 0,
     });
     const [to, setTo] = useState({
-        area: ''
+        area: '',
+        street: '',
+        city: '',
+        longitude: 0,
+        latitude: 0,
     })
     const [transportations, setTransportations] = useState([]);
+    const [fromValue, setFromValue] = useState('');
+    const [toValue, setToValue] = useState('');
+    const [suggested, setSuggested] = useState([]);
+    const [alert, setAlert] = useState({
+        message: '',
+        type: null
+    });
 
     useEffect(() => {
         getAvailableTransportations().then(transportations => {
-            console.log(transportations);
             setTransportations(transportations);
-        })
+        });
+        console.log(suggested);
     }, []);
 
-    const districts = [
-        "Abbassia",
-        "Ain Shams",
-        "Azbakeya",
-        "Bab al-Louq",
-        "Boulaq",
-        "City of the Dead (Cairo)",
-        "Coptic Cairo",
-        "Daher, Egypt",
-        "Downtown Cairo",
-        "El Manial",
-        "El Marg",
-        "El Matareya, Cairo",
-        "El Qobbah",
-        "El Rehab",
-        "El Sahel",
-        "El Sakkakini",
-        "Ezbet El Haggana",
-        "Ezbet El Nakhl",
-        "Faggala",
-        "Fifth Settlement",
-        "Fustat",
-        "Garden City, Cairo",
-        "Gezira (Cairo)",
-        "Heliopolis, Cairo",
-        "Islamic Cairo",
-        "Maadi",
-        "Old Cairo",
-        "Roda Island",
-        "Shubra (administrative region)",
-        "Shubra",
-        "Shubra El Kheima",
-        "Wagh El Birket",
-        "Zamalek",
-        "Zeitoun, Cairo"
-    ]
-
     const insertFrom = event => {
-        setFrom({
-            ...from,
-            area: event.target.value
-        })
+        const input = event.target.value;
+
+        setFromValue(event.target.value);
+
+        if(input.length >= 3 && input[input.length-1] !== ' '){
+            getGeoLocation(input).then(data => {
+                if(data.area) setFrom({
+                    ...from,
+                    area: data.area,
+                    city: data.city,
+                    longitude: data.longitude,
+                    latitude: data.latitude,
+                });
+
+                if(data.area && !suggested.includes(data.area)){
+                    setSuggested([...suggested, data.area]);
+                    setFromValue(data.area);
+                }
+            })
+        }
     }
 
     const insertTo = event => {
-        setTo({
-            ...from,
-            area: event.target.value
-        })
+        const input = event.target.value;
+
+        setToValue(event.target.value);
+
+        if(input.length >= 3 && input[input.length-1] !== ' '){
+            getGeoLocation(input).then(data => {
+                if(data.area) setTo({
+                    ...to,
+                    area: data.area,
+                    city: data.city,
+                    longitude: data.longitude,
+                    latitude: data.latitude,
+                });
+
+                if(data.area && !suggested.includes(data.area)){
+                    setSuggested([...suggested, data.area]);
+                    setToValue(data.area);
+                }
+            })
+        }
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        if(!item || !amount || !description || !transportation || !from || !to)
+            setAlert({
+                message: 'Check required inputs',
+                type: 'error'
+            })
     }
 
     return (
         <div className="delivery-form">
             <form onSubmit={handleSubmit}>
                 <div className="form_container">
-                    <input type="text" placeholder="From" value={from.area} onChange={insertFrom} list="from-list"/>
-                    <datalist id="from-list">
-                        <option value="Boston"/>
+                    <input type="text" placeholder="From" value={fromValue} onChange={insertFrom} list="from" style={{border: alert.type === 'error' && !from.area && '1px red solid'}}/>
+                    <datalist id="from">
+                        <option key="source" value={from.area}/>
                     </datalist>
-                    <input type="text" placeholder="To" value={to.area} onChange={insertTo} list="to-list"/>
+                    <input type="text" placeholder="To" value={toValue} onChange={insertTo} list="to-list" style={{border: alert.type === 'error' && !to.area && '1px red solid'}}/>
                     <datalist id="to-list">
-                        <option value="Boston"/>
+                        <option key="destination" value={to.area}/>
                     </datalist>
-                    <input type="number" placeholder="Amount" value={amount} onChange={event => setAmount(event.target.value)} />
-                    <select name="author" id="author" value={transportation} onChange={event => setTransportation(event.target.value)} >
-                        <option disabled selected value="Transportation way">Transportation way</option>
+                    <input type="number" placeholder="Amount" value={amount} onChange={event => setAmount(event.target.value)} style={{border: alert.type === 'error' && !amount && '1px red solid'}}/>
+                    <select name="author" id="author" value={transportation} onChange={event => setTransportation(event.target.value)} style={{border: alert.type === 'error' && !transportation && '1px red solid'}}>
+                        <option disabled selected value="">Transportation way</option>
                         {
                             transportations.map(transportation => {
                                 return <option value={transportation}>{transportation}</option>;
                             })
                         }
                     </select>
-                    <input type="text" placeholder="Item to deliver" value={item} onChange={event => setItem(event.target.value)} />
-                    <textarea placeholder="More info" value={description} onChange={event => setDescription(event.target.value)}/>
+                    <input type="text" placeholder="Item to deliver" value={item} onChange={event => setItem(event.target.value)} style={{border: alert.type === 'error' && !item && '1px red solid'}}/>
+                    <textarea placeholder="More info" value={description} onChange={event => setDescription(event.target.value)} style={{border: alert.type === 'error' && !description && '1px red solid'}}/>
                     <button type="submit" className="submit-btn">Submit</button>
                 </div>
             </form>
---
+++ client/src/endpoints/geocoding.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const bing_key = `${process.env.REACT_APP_BING_KEY}`;
+
+export const getGeoLocation = (input) => {
+    const url = `http://dev.virtualearth.net/REST/v1/Locations/egypt%20${input}?o=json&key=${bing_key}`;
+
+    return axios.get(url).then(res => {
+        if(res.data){
+            const data = {
+                area: res.data.resourceSets[0].resources[0].address.addressLine,
+                city: res.data.resourceSets[0].resources[0].address.adminDistrict,
+                latitude: res.data.resourceSets[0].resources[0].point.coordinates[0],
+                longitude: res.data.resourceSets[0].resources[0].point.coordinates[1],
+            }
+
+            return data
+        }
+    }).catch(err => err);
+}
