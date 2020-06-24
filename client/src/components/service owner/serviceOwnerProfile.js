46a46c521e9d25447fc4baa9536e1ecfbf9bf6c8
ali-ismail-g <ali.ismail.ghareeb@gmail.com>
2020-06-24 02:10:34 +0200

    modifying serviceowner profile frontend

--- client/src/components/service owner/serviceOwnerProfile.js
+++ client/src/components/service owner/serviceOwnerProfile.js
@@ -9,22 +9,28 @@ const ServiceOwnerProfile =({match})=>{
     const domain = `${process.env.REACT_APP_BACKEND_DOMAIN}`
     const API_SERVICEOWNER_URL = `${domain}/service-owners/${serviceOwnerId}`;
     const [serviceOwner, setServiceOwner] = useState([]);
+    const [user, setUser] = useState([]);
     const [productOwner, setProductOwner] = useState([]);
-    const [serviceDistance, setServiceDistance] = useState("");
-    const [serviceRegion, setServiceRegion] = useState("");
-    const [serviceTransportation, setServiceTransportation] = useState("");
-    const [serviceRating, setServiceRating] = useState("");
+    // const [serviceDistance, setServiceDistance] = useState("");
+    // const [serviceRegion, setServiceRegion] = useState("");
+    // const [serviceTransportation, setServiceTransportation] = useState("");
+    // const [serviceRating, setServiceRating] = useState("");
+    const [imagePath,setImagePath ] = useState("");
     const [modal, setModal] = useState(false);
+    const [serviceObj,setServiceObj] = useState({
+        distance:0,region:0,transportation:"",rating:0
+    })
     const toggle = () => setModal(!modal);
     console.log(serviceOwnerId);
     useEffect(() => {
-        axios.get('http://localhost:5000/service-owners/5ee65308b60c072b27451ab9', authHeader)
+        axios.get('http://localhost:5000/service-owners/5ef284d743ed7e1916f4aa22', authHeader)
     .then(response => {
-
         console.log("success");
         console.log("serviceO",response.data);
-        setServiceOwner(response.data)
-        // setProductOwner(response.data.productOwner)
+        setServiceOwner(response.data);
+        setUser(response.data.user);
+        setServiceObj(response.data);
+        setProductOwner(response.data.productOwner);
     })
     .catch(err => {
         if (err.response) {
@@ -46,36 +52,48 @@ const ServiceOwnerProfile =({match})=>{
         float: "right",
         borderRadius: "8px"
     }
-
+    
     const updateServiceOwner = async(e)=>{
         e.preventDefault();
         let forum = new FormData()
-        forum.append('distance', serviceDistance)
-        forum.append('region',serviceRegion)
-        forum.append('transportation',serviceTransportation)
-        forum.append('rating',serviceRating)
+        forum.set('distance', serviceObj.distance)
+        forum.set('region',serviceObj.region)
+        forum.set('transportation',serviceObj.transportation)
+        forum.set('rating',serviceObj.serviceRating)
         try {
-            console.log(serviceDistance);
+            console.log(serviceObj);
             // console.log(forum.get('image_path'));
-            const response = await axios.patch(
-                `http://localhost:5000/service-owners/5ee65308b60c072b27451ab9`,
+            const res = await axios.patch(
+                `http://localhost:5000/service-owners/5ef284d743ed7e1916f4aa22`,
                 forum , authHeader
             );
-            console.log(serviceOwner);
-            setServiceOwner(owner=>owner.concat(response.data));
+            // console.log(res);
+            // console.log(serviceOwner);
+            // console.log(forum);
+            console.log(res.data);
+            setServiceObj(res.data);
             toggle();
-            console.log(serviceOwner);
-        } catch (error) {console.log(error)}
-       
+            console.log(serviceObj);
+        } catch (error) {console.log(error)}  
     }
-    
+    const statusChange=async(e)=>{
+        e.preventDefault();
+        try{
+        const response = await axios.get(
+            `http://localhost:5000/service-owners/5ef284d743ed7e1916f4aa22/status`,
+             authHeader
+        );
+        setUser(response.data.user)
+        console.log(response);
+        }catch(error){console.log(error)}
+    }
     
     
     return(
         <div>
            <Card key={serviceOwner._id} style={{ width: "20rem", marginLeft: "22rem", marginTop: "2rem" }} >
               <CardBody>
-              <CardImg />
+              <CardImg src="http://localhost:5000/uploads/users/images/2020-06-23T16:35:20.178Z-64329874_2086624524776682_8448602010156007424_n.jpg" alt="serviceOwner Pic" style={{ width: "100px", height: "100px" }}/>
                 <CardTitle>
                     Service Owner Profile
                 </CardTitle>
@@ -91,20 +109,23 @@ const ServiceOwnerProfile =({match})=>{
                  <CardText>
                     Ratings:{serviceOwner.rating}
                  </CardText>
+                 <CardText>
+                    Status:{user.status}
+                 </CardText>
+                 
                </CardBody>
-               <button style={buttonStyle} onClick={toggle} className="bg-dark text-white"> 
-                <FontAwesomeIcon icon={faPlusCircle} />
-            </button>
+               <button onClick={toggle} className="btn btn-primary"> Update</button>
+               <button onClick={toggle} className="btn btn-success" onClick={statusChange}> Change Status</button>
              </Card>
              <Modal isOpen={modal} toggle={toggle}>
         <form encType="multipart/form-data" onSubmit={(e)=>{updateServiceOwner(e)}}>
             <ModalHeader className="bg-dark text-white">Update Service Owner Data</ModalHeader>
             <ModalBody>
                 <FormGroup>
-                    <Input type="number" value={serviceDistance} onChange={(e)=>{setServiceDistance(e.target.value)}} placeholder="Distance" /><br/>
-                    <Input type="number" value={serviceRegion} onChange={(e)=>{setServiceRegion(e.target.value)}}  placeholder="Region"/><br/>
-                    <Input type="text" value={serviceTransportation} onChange={(e)=>{setServiceTransportation(e.target.value)}} placeholder="Transportation"/><br/>
-                    <Input type="number" value={serviceRating} onChange={(e)=>{setServiceRating(e.target.value)}} placeholder="Rating"/><br/>
+                    <Input type="number" value={serviceObj.distance} onChange={(e)=>{setServiceObj({...serviceObj,distance:e.target.value})}} placeholder="Distance" /><br/>
+                    <Input type="number" value={serviceObj.region} onChange={(e)=>{setServiceObj({...serviceObj,region:e.target.value})}}  placeholder="Region"/><br/>
+                    <Input type="text" value={serviceObj.transportation} onChange={(e)=>{setServiceObj({...serviceObj,transportation:e.target.value})}} placeholder="Transportation"/><br/>
+                    <Input type="number" value={serviceObj.rating} onChange={(e)=>{setServiceObj({...serviceObj,rating:e.target.value})}} placeholder="Rating"/><br/>
                     {/* <Input type="file" name = "image_path" onChange={imageChange} placeholder="image"/><br/> */}
                 </FormGroup>
             </ModalBody>
--- server/controllers/serviceOwners.js
+++ server/controllers/serviceOwners.js
@@ -33,6 +33,7 @@ const changeStatus = async(req,res)=>{
 const getServiceOwner = async(req,res)=>{
     try{
         let user =await ServiceOwner.findById({_id: req.params.id})
+        .populate('user');
         res.send(user)
         }
         catch(err){
@@ -41,9 +42,12 @@ const getServiceOwner = async(req,res)=>{
         }
 }
 const updateServiceOwner = (req,res)=>{
+    console.log(req.body);
     ServiceOwner.findOneAndUpdate({_id: req.params.id},req.body,{new: true},(error,user)=>{
         res.status(200).json({"data": user});
-    }).catch((err) => {
+    })
+    .populate('user')
+    .catch((err) => {
         console.log(err);
         res.status(400).json({"error": err});
     })
@@ -254,4 +258,5 @@ module.exports = {
     getProductOwnerDetails,
     filteredServiceOwners,
     deliverNewProduct
+    
 }
